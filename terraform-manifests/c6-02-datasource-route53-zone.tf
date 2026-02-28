ffc1451c4451a1e81837c9e1d9e0a6cc2255f15c
sureshaho <40929036+sureshaho@users.noreply.github.com>
2026-02-28 15:20:46 +0530

    Update c6-02-datasource-route53-zone.tf

--- terraform-manifests/c6-02-datasource-route53-zone.tf
+++ terraform-manifests/c6-02-datasource-route53-zone.tf
@@ -1,6 +1,6 @@
 # Get DNS information from AWS Route53
 data "aws_route53_zone" "mydomain" {
-  name         = "devopsincloud.com"
+  name         = "rayaru-sure-saru.cloud"
 }
 
 # Output MyDomain Zone ID
